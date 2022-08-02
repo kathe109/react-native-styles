7d1d877c0eea80db85cbe5a773814900858cbbca
katherine rueda <kathkien24@gmail.com>
2022-08-01 19:07:10 -0600

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={[styles.welcome, {backgroundColor: 'blue'
+      } ,{color: 'white'}, {flex: 1}]}>Hello world!</Text>
+      <Text style={[styles.welcome, {backgroundColor: 'white'
+      }, {flex: 1}]}>Hello world!</Text>
+      <Text style={[styles.welcome, {backgroundColor: 'red'
+      }, {flex: 3}]}>Hello world!</Text>
+      <Text style={[styles.welcome, {backgroundColor: 'white'
+      }, {flex: 1}]}>Hello world!</Text>
+      <Text style={[styles.welcome, {backgroundColor: 'blue'
+      }, {color: 'white'}, {flex: 1}]}>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: 'orange',
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+    flexDirection: 'row',
+    margin: 10,
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    // margin: 10,
+  }
 });
